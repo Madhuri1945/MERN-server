c25186af943fb73b6933875cd20e44ca42e213ca
Madhuri1945 <madhuridhulipudi37@gmail.com>
2024-07-08 10:27:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const cors=require('cors')
+const {ApolloServer,gql}=require('apollo-server-express')
+const typeDefs=require('./schema')
+const resolvers=require('./resolvers')
+const userApiFromRouter=require('./routes/userRoutes');
+const port=3001
+const app=express()
+app.use(express.json())
+app.use(cors())
+//const userApiFromRouter=require('./routes/userRoutes');
+const mongoURI = 'mongodb+srv://madhuridhulipudi19:g8CFQaP521fhA65p@cluster0.3kpxupq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; 
+mongoose.connect(mongoURI,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>console.log('Mongodb connected')).catch(err=>console.log(err));
+const server=new ApolloServer({typeDefs,resolvers});
+
+async function StartServer(){
+    await server.start();
+    server.applyMiddleware({app});
+    app.use('/users',userApiFromRouter)
+    app.listen(port,()=>{console.log("server is live")})
+
+}
+StartServer()
+
